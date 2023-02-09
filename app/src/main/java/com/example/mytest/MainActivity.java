951d35be608a83e613cc56b4e214b7d066a71b7c
zhangchaojun <565978459@qq.com>
2023-02-09 11:03:27 +0800

    整理项目

--- app/src/main/java/com/example/mytest/MainActivity.java
+++ app/src/main/java/com/example/mytest/MainActivity.java
@@ -2,20 +2,13 @@ package com.example.mytest;
 
 import android.view.View;
 
-
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import com.cj.library.base.BaseActivity;
-import com.example.myfragmentlifecycle.R;
+import com.example.myshareduserid.R;
 
-/**
- * androidx中的fragment
- */
+
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = "cj";
-    private Fragment1 fragment1;
 
 
     @Override
@@ -25,10 +18,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     @Override
     protected void initView() {
-        FragmentManager supportFragmentManager = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
-        fragment1 = new Fragment1(R.layout.fragment_1);
-        fragmentTransaction.add(fragment1,"fragment1").show(fragment1).commit();
+
     }
 
 
